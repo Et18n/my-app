0ef5da49f07cec112059808de4498ac42fbb89ad
Et18n <87770677+Et18n@users.noreply.github.com>
2024-01-28 23:50:41 +0530

    Revert "d1"
    This reverts commit 35693013da4d958f5b290ab02b09c1705c58c079.

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,23 +1,3 @@
 export default function Register(){
-    return (
-        <>
-         <div className="body">
-            <div className="container">
-                <div id="title">Signup</div>
-                <p id="wlcm"> Hello New User </p>
-                Enter your Username and Password
-                <div className="user">
-                Username<input type="text" id="user" /></div>
-                <div className="pass">
-                Password <input type="password"  id="pass" minLength={4} maxLength={7}  />
-                </div>
-                <input type="button" value="Sign Up" id="submit"  />
-            </div>
-        </div>
-        
-        
-        </>
-
-
-    );
+    return <h1> Register</h1>
 }
