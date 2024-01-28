35693013da4d958f5b290ab02b09c1705c58c079
Et18n <87770677+Et18n@users.noreply.github.com>
2024-01-28 23:47:23 +0530

    d1

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,3 +1,23 @@
 export default function Register(){
-    return <h1> Register</h1>
+    return (
+        <>
+         <div className="body">
+            <div className="container">
+                <div id="title">Signup</div>
+                <p id="wlcm"> Hello New User </p>
+                Enter your Username and Password
+                <div className="user">
+                Username<input type="text" id="user" /></div>
+                <div className="pass">
+                Password <input type="password"  id="pass" minLength={4} maxLength={7}  />
+                </div>
+                <input type="button" value="Sign Up" id="submit"  />
+            </div>
+        </div>
+        
+        
+        </>
+
+
+    );
 }
